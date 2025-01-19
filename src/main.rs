2f88c4ba0425aaf88147111af463d578ca6b6805
Afaan Bilal <afaanbilal@gmail.com>
2025-01-19 09:23:29 +0530

    ✨ Rate Limiting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::time::Duration;
+
+use actix_extensible_rate_limit::{
+    backend::{memory::InMemoryBackend, SimpleInputFunctionBuilder},
+    RateLimiter,
+};
 /**
  * 💵 MoneyFlow
  *
@@ -31,8 +37,20 @@ async fn main() -> std::io::Result<()> {
         jwt_secret: std::env::var("JWT_SECRET").unwrap(),
     });
 
+    let rate_limiter_backend = InMemoryBackend::builder().build();
+
     HttpServer::new(move || {
         App::new()
+            .wrap(
+                RateLimiter::builder(
+                    rate_limiter_backend.clone(),
+                    SimpleInputFunctionBuilder::new(Duration::from_secs(60), 50)
+                        .real_ip_key()
+                        .build(),
+                )
+                .add_headers()
+                .build(),
+            )
             .app_data(state.clone())
             .service(controllers::auth::sign_up)
             .service(controllers::auth::sign_in)
